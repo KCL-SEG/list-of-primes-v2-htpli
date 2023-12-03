479cf17656c4758798ee3981fc78c722b44c52fe
Humayra Tapali <humayra.tapali@kcl.ac.uk>
2023-12-03 21:05:27 +0000

    Prime function

--- primes.py
+++ primes.py
@@ -2,5 +2,19 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    if number_of_primes <= 0:
+        raise ValueError("number_of_primes must be a positive integer")
+
     list = []
+    num = 2
+    while len(list) < number_of_primes:
+        is_prime = True
+        for i in range(2, int(num ** 0.5) + 1):
+            if num % i == 0:
+                is_prime = False
+                break
+        if is_prime:
+            list.append(num)
+        num += 1
+
     return list
